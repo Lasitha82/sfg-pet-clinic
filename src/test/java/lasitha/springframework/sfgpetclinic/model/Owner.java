41319652fcd3b98bdb046c479214b49fd4ab4af4
Lasitha Priyankara <priyankara.lasitha@meta.co.jp>
2021-10-25 17:09:49 +0900

    adding POSOs. Closes #2

---
+++ src/test/java/lasitha/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package lasitha.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/test/java/lasitha/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package lasitha.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
